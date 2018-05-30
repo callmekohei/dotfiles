211c20c54961a95e3593e405006ce917131e2b11
callmekohei <callmekohei@gmail.com>
2018-05-30 21:56:20 +0900

    add .gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -8,3 +8,5 @@ ln -sf ~/dotfiles/init.vim       ~/.vimrc
 ln -sf ~/dotfiles/init.vim       ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/dein.toml      ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
