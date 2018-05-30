da1ceadc648cd86d4042a933dd179fa05e67e319
callmekohei <callmekohei@gmail.com>
2018-05-30 22:16:14 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bin
+#!/usr/bin/env sh
 
-ln -sf ~/dotfiles/.bashrc       ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.inputrc      ~/.inputrc
-
+ln -sf ~/dotfiles/.bashrc        ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile  ~/.bash_profile
+ln -sf ~/dotfiles/.inputrc       ~/.inputrc
+ln -sf ~/dotfiles/.gitconfig     ~/.gitconfig
 ln -sf ~/dotfiles/init.vim       ~/.vimrc
 ln -sf ~/dotfiles/init.vim       ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/dein.toml      ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
 
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
