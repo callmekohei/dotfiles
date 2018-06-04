49dac04b2291f2091c422642efff33e7423979a8
callmekohei <callmekohei@gmail.com>
2018-06-04 16:01:55 +0900

    fix

--- dotfilelinker.bash
+++ dotfilelinker.bash
@@ -9,7 +9,7 @@ ln -sf ${FROMDIR}/bash/profile ${DISTDIR}/.bash_profile
 ln -sf ${FROMDIR}/bash/inputrc ${DISTDIR}/.inputrc
 
 # Git/Github
-ln -sf ${FROMDIR}/git/gitconfig ${DISTDIR}/.gitconfig
+ln -sf ${FROMDIR}/git/.gitconfig ${DISTDIR}/.gitconfig
 
 # Vim/Neovim
 ln -sf ${FROMDIR}/vim/vimrc                ${DISTDIR}/.vimrc
