2573d391745484b38f77cea71a96693bb3789394
callmekohei <callmekohei@gmail.com>
2019-01-07 03:13:53 +0900

    fix

---
+++ vim/foo.bash
@@ -0,0 +1,9 @@
+cd $HOME/.vim/pack/pack1/start
+
+git clone https://github.com/romainl/Apprentice
+git clone tomtom/tcomment_vim
+git clone https://github.com/nathanaelkane/vim-indent-guides
+git clone https://github.com/junegunn/vim-easy-align
+git clone https://github.com/thinca/vim-quickrun
+git clone https://github.com/jonathanfilip/vim-lucius
+
