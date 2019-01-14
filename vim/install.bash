4994ce4ca9f737e632aa1fe46bceeb428458591e
callmekohei <callmekohei@gmail.com>
2019-01-14 23:31:13 +0900

    fix

--- vim/install.bash
+++ vim/install.bash
@@ -6,10 +6,18 @@ git clone https://github.com/junegunn/vim-easy-align
 git clone https://github.com/thinca/vim-quickrun
 git clone https://github.com/jonathanfilip/vim-lucius
 git clone https://github.com/ctrlpvim/ctrlp.vim
-git clone https://github.com/callmekohei/memolist.vim
-cd memolist.vim
-git checkout use_grep
 
 # Git/Github
 git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/tpope/vim-fugitive
+
+# SQL
+git clone https://github.com/vim-scripts/dbext.vim
+git clone https://github.com/vim-scripts/SQLUtilities
+git clone https://github.com/vim-scripts/Align
+
+# memolist( modified )
+rm -rf memolist.vim
+git clone https://github.com/callmekohei/memolist.vim
+$(cd memolist.vim ; git checkout use_ctrlp)
+pwd
