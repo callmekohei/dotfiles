35ab3f06a34128077355d4653c50ebe5576d33c7
callmekohei <callmekohei@gmail.com>
2019-01-20 11:57:23 +0900

    fix

--- vim/install.bash
+++ vim/install.bash
@@ -1,14 +1,18 @@
-cd $HOME/.vim/pack/pack1/start
+### --------------------------------------------------------
+###                  git clone vim plugins
+### --------------------------------------------------------
+
+if [ ! -d pack/pack1/start/ ]; then
+  mkdir -p pack/pack1/start
+fi
+
+cd pack/pack1/start
 
 git clone https://github.com/romainl/Apprentice
 git clone https://github.com/nathanaelkane/vim-indent-guides
 git clone https://github.com/junegunn/vim-easy-align
 git clone https://github.com/thinca/vim-quickrun
-git clone https://github.com/jonathanfilip/vim-lucius
 git clone https://github.com/ctrlpvim/ctrlp.vim
-
-# Git/Github
-# git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/tpope/vim-fugitive
 
 # SQL
@@ -21,3 +25,6 @@ rm -rf memolist.vim
 git clone https://github.com/callmekohei/memolist.vim
 $(cd memolist.vim ; git checkout use_ctrlp)
 pwd
+
+# vb
+git clone https://github.com/callmekohei/vbnet.vim
